7d991ca97f3affc6f3e40c6ad3d03f69eff2b714
Kadi Kraman <hellokadi@gmail.com>
2026-03-02 15:43:20 +0000

    Configure observe

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,19 @@
+import { useEffect } from 'react';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import AppMetrics from 'expo-eas-observe';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    AppMetrics.markFirstRender();
+    AppMetrics.markInteractive();
+  }, []);
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
